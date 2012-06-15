70750c54c0cd64562b32ec01da1b0e1a5c05a2e9
JustusW <justus@abi007.info>
2012-06-15 13:43:55 +0200

    Switched to jQuery, started structural change

--- basdem.js
+++ basdem.js
@@ -5,21 +5,12 @@ var Window = new function() {
 	this.footer = null;
 
 	this.create = function() {
-		this.container = document.createElement("div");
-		this.headline = document.createElement("div");
-		this.content = document.createElement("div");
-		this.footer = document.createElement("div");
+		this.container = $("<div class=\"container\">");
+		this.headline = $("<div class=\"headline\">").appendTo(this.container);
+		this.content = $("<div class=\"content\">").appendTo(this.container);
+		this.footer = $("<div class=\"footer\">").appendTo(this.container);
 		
-		this.container.setAttribute("class","container");
-		this.headline.setAttribute("class","headline");
-		this.content.setAttribute("class","content");
-		this.footer.setAttribute("class","footer");
-		
-		this.container.appendChild(this.headline);
-		this.container.appendChild(this.content);
-		this.container.appendChild(this.footer);
-		
-		document.body.appendChild(this.container);
+		this.container.appendTo("body");
 	};
 	
 	this.destroy = function() {
@@ -29,11 +20,22 @@ var Window = new function() {
 
 var BasDeM = new function() {
 	this.start = function() {
-		Window.create();
-		TopicList.load();
-		TopicList.populate();
+		BasDeM.authenticate();
 	}
-
+	
+	this.authenticate = function() {
+		// Todo: Do authentication and then proceed
+		this.proceed();
+	}
+	
+	this.proceed = function() {
+		// Todo
+		
+	}
+	
+	this.load = function(id) {
+		// Todo: Load a specific id
+	}
 };
 
 var TopicList = new function() {
@@ -43,20 +45,16 @@ var TopicList = new function() {
 	
 	this.populate = function() {
 		for ( i in this.content ) {
-			Window.content.appendChild(Topic.create(this.content[i]));
+			Topic.create(this.content[i]).appendTo(Window.content);
 		}
-		var linebreak = document.createElement("br");
-		linebreak.setAttribute("class","clear");
-		Window.content.appendChild(linebreak);
+		$("<br class=\"clear\">").appendTo(Window.content);
 	}
 
 };
 
 var Topic = new function() {
 	this.create = function(content) {
-		var tmp = document.createElement("div");
-		tmp.setAttribute("class","topic");
-		tmp.innerHTML = "id:"+content.id+"-title:"+content.title+"-author:"+content.author;
+		tmp = $("<div class=\"topic\">" + "id:"+content.id+"-title:"+content.title+"-author:"+content.author + "</div>");
 		return tmp;
 	}
 
