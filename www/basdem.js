cefea1439868e4254c522f57eb2d303acf012167
Kevin Honka <kevin.honka@hotmail.de>
2012-07-02 10:57:36 +0200

    some basic changes

--- www/basdem.js
+++ www/basdem.js
@@ -1,20 +1,37 @@
-var Window = new function() {
+var View = new function(Memplex) {
 	this.container = null;
 	this.headline = null;
 	this.content = null;
 	this.footer = null;
-
+	
 	this.create = function() {
 		this.container = $("<div class=\"container\">");
 		this.headline = $("<div class=\"headline\">").appendTo(this.container);
 		this.content = $("<div class=\"content\">").appendTo(this.container);
+		this.content-left = $("<div class=\"content-left\">").appendTo(this.container);
+		this.content-right = $("<div class=\"content-right\">").appendTo(this.container);
 		this.footer = $("<div class=\"footer\">").appendTo(this.container);
 		
 		this.container.appendTo("body");
 	};
 	
-	this.destroy = function() {
+	if(Layer = "1" || Layer = "2"){}
+	else if(Layer = "3"){}
+	else if(Layer = "4" || Layer = "5" || Layer = "6"){}
+	else if(Layer = "7"){}
+	
+	
+	
+	this.clear = function() {
 		document.body.removeChild(this.container);
+	}
+	
+	private this.block() {
+		Window.block();
+	}
+	
+	this.destroy = function() {
+		
 	};
 };
 
@@ -37,6 +54,20 @@ var BasDeM = new function() {
 		// Todo: Load a specific id
 	}
 };
+
+var ViewList = new function(Memplex) {
+	TopicList.load();
+	TopicList.populate();
+
+
+}
+
+var ViewSolution = new function(Memplex) {
+}
+
+var ViewComment =new function(Memplex) {
+}
+
 
 var TopicList = new function() {
 	this.load = function() {
