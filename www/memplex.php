89365352044fea21b74d6ad590430f45cafd1802
JustusW <justus@abi007.info>
2012-07-13 23:24:50 +0200

    Cleaning up. Comments are for wussies!

--- www/memplex.php
+++ www/memplex.php
@@ -19,7 +19,7 @@
 
 define('INCMS',true);
 define('NL',"<br>\r\n");
-#error_reporting("E_ALL & ~E_DEPRECATED & ~E_STRICT");
+error_reporting("E_ALL & ~E_DEPRECATED & ~E_STRICT");
 
 require_once('class/config.class.php');
 require_once('class/mysql.class.php');
