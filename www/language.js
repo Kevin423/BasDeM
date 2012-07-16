b5446aef884c7c15f01a75cb528a325a66e646fe
JustusW <justus@abi007.info>
2012-07-16 15:25:15 +0200

    bugfixes

--- www/language.js
+++ www/language.js
@@ -2,23 +2,23 @@ var Language = new function() {
 this.get = function(target,lang) {
 switch ( lang ) {
     case 'de':
-        switch ( target ) {
+        switch ( target.toLowerCase() ) {
             case 'lang_issue': return 'Problem'; break;
-            case 'lang_CreateTopic': return 'Thema anlegen'; break;
-            case 'lang_CreateIssue': return 'Problem anlegen'; break;
-            case 'lang_CreateSolution': return 'L&ouml;sung anlegen'; break;
-            case 'lang_CreateArgument': return 'Argument anlegen'; break;
-            case 'lang_CreateComment': return 'Kommentar anlegen'; break;
-            case 'lang_Topic': return 'Thema'; break;
-            case 'lang_Solution': return 'L&ouml;sung'; break;
-            case 'lang_Author': return 'Autor'; break;
-            case 'lang_Description': return 'Beschreibung'; break;
-            case 'lang_Title': return 'Titel'; break;
-            case 'lang_Cancel': return 'Abbrechen'; break;
-            case 'lang_Back': return 'zur&uuml;ck'; break;
-            case 'lang_Pro': return 'Pro'; break;
-            case 'lang_Contra': return 'Contra'; break;
-            case 'lang_Neutral': return 'Neutral'; break;
+            case 'lang_createtopic': return 'Thema anlegen'; break;
+            case 'lang_createissue': return 'Problem anlegen'; break;
+            case 'lang_createsolution': return 'L&ouml;sung anlegen'; break;
+            case 'lang_createargument': return 'Argument anlegen'; break;
+            case 'lang_createcomment': return 'Kommentar anlegen'; break;
+            case 'lang_topic': return 'Thema'; break;
+            case 'lang_solution': return 'L&ouml;sung'; break;
+            case 'lang_author': return 'Autor'; break;
+            case 'lang_description': return 'Beschreibung'; break;
+            case 'lang_title': return 'Titel'; break;
+            case 'lang_cancel': return 'Abbrechen'; break;
+            case 'lang_back': return 'zur&uuml;ck'; break;
+            case 'lang_pro': return 'Pro'; break;
+            case 'lang_contra': return 'Contra'; break;
+            case 'lang_neutral': return 'Neutral'; break;
         }; break;
     }
 };
