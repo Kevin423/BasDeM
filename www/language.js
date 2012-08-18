7a0ff876d08343673162b48104f281fde4d46200
JustusW <justus@abi007.info>
2012-08-18 20:56:22 +0200

    Inserted lang_list* for left list. removed breaks after return, seriously guys?

--- www/language.js
+++ www/language.js
@@ -1,96 +1,121 @@
+/** It's all Svens fault!!1!11 **********************************************************
+ * Copyright (c) 2012 Justus Wingert <justus_wingert@web.de>                            *
+ *                                                                                      *
+ * This file is part of BasDeM.                                                         *
+ *                                                                                      *
+ * BasDeM is free software; you can redistribute it and/or modify it under              *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 3 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * BasDeM is distributed in the hope that it will be useful, but WITHOUT ANY            *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * BasDeM. If not, see <http://www.gnu.org/licenses/>.                                  *
+ ****************************************************************************************/
+ 
+ /* Important: de_DE is the default language, add there under all circumstances! */
+
 var Language = new function() {
 this.get = function(target,lang) {
 switch ( target ) {
     case 'de_DE':
         switch ( lang ) {
-            case 'lang_allFilter': return 'Beitrag erf&uuml;llt alle hier ausgew&auml;hlten Filter:'; break;
-            case 'lang_answer': return 'Antworten'; break;
-            case 'lang_argPro': return 'Pro Argument hinzuf&uuml;gen'; break;
-            case 'lang_argNeut': return 'Neue Antwort hinzuf&uuml;gen'; break;
-            case 'lang_argCon': return 'Contra Argument hinzuf&uuml;gen'; break;
-            case 'lang_author': return 'Autor'; break;
-            case 'lang_back': return 'zur&uuml;ck'; break;
-            case 'lang_cancel': return 'Abbrechen'; break;
-            case 'lang_createArgumentPro': return 'Neues Pro Argument erstellen'; break;
-            case 'lang_createArgumentNeut': return 'Neue Antwort erstellen'; break;
-            case 'lang_createArgumentCon': return 'Neues Contra Argument erstellen'; break;
-            case 'lang_createcomment': return 'Kommentar anlegen'; break;
-            case 'lang_createsolution': return 'L&ouml;sung anlegen'; break;
-            case 'lang_debate': return 'Debatten'; break;
-            case 'lang_description': return 'Beschreibung'; break;
+            case 'lang_allFilter': return 'Beitrag erf&uuml;llt alle hier ausgew&auml;hlten Filter:';
+            case 'lang_answer': return 'Antworten';
+            case 'lang_argPro': return 'Pro Argument hinzuf&uuml;gen';
+            case 'lang_argNeut': return 'Neue Antwort hinzuf&uuml;gen';
+            case 'lang_argCon': return 'Contra Argument hinzuf&uuml;gen';
+            case 'lang_author': return 'Autor';
+            case 'lang_back': return 'zur&uuml;ck';
+            case 'lang_cancel': return 'Abbrechen';
+            case 'lang_createArgumentPro': return 'Neues Pro Argument erstellen';
+            case 'lang_createArgumentNeut': return 'Neue Antwort erstellen';
+            case 'lang_createArgumentCon': return 'Neues Contra Argument erstellen';
+            case 'lang_createcomment': return 'Kommentar anlegen';
+            case 'lang_createsolution': return 'L&ouml;sung anlegen';
+            case 'lang_debate': return 'Debatten';
+            case 'lang_description': return 'Beschreibung';
             case 'lang_errorPartent':
-            case 'lang_errorLayer': return 'Es ist ein schwerer Fehler aufgetreten. Bitte Klicke auf Abbrechen!'; break;
-            case 'lang_errorDebateTitle': return 'Bitte gib einen Titel f&uuml;r deine Debatte an!'; break;
-            case 'lang_filter': return 'W&auml;hle die gew&uuml;nschten Filter aus'; break;
-            case 'lang_filterSelect': return 'Filter einstellen'; break;
-            case 'lang_helpNewComment': return '<p>Bitte beschreibe dein neues Argument sachlich und verst&auml;ndlich und wiederhole keine Punkte, die bereits genannt wurden.</p><p>Der <b>Titel</b> ist die Kurzfassung deines Argumentes in wenigen Worten. Details und Quellen kannst du in der <b>Beschreibung</b> erl&auml;utern. Du kannst auch Argumente hinzuf&uuml;gen, die nicht deine pers&ouml;nliche Meinung st&uuml;tzen.</p>'; break;
-            case 'lang_helpNewDebate': return '<p>Bitte beschreibe deine neue Debatte sachlich, neutral und verst&auml;ndlich und stelle sicher, dass diese Debatte nicht bereits gef&uuml;hrt wird.</p><p>Der <b>Titel</b> ist die Kurzfassung der Debatte in wenigen Worten. Details und Quellen kannst du in der <b>Beschreibung</b> erl&auml;utern. Bitte f&uuml;ge die Debatte auch einem oder mehreren <b>Themenbereichen</b> hinzu, sodass sie leicht gefunden werden kann.</p>'; break;
-            case 'lang_helpNewSolution': return '<p>Bitte beschreibe deine neue L&ouml;sung sachlich, neutral und verst&auml;ndlich und wiederhole keine L&ouml;sungen, die bereits genannt wurden.</p><p>Der <b>Titel</b> ist die Kurzbeschreibung deiner L&ouml;sung in wenigen Worten. Details und Quellen kannst du in der <b>Beschreibung</b> erl&auml;utern. Du kannst auch L&ouml;sungen hinzuf&uuml;gen, die du pers&ouml;nlich nicht unterst&uuml;tzt.</p>'; break;
-            case 'lang_logout': return 'Logout'; break;
-            case 'lang_minFilter': return 'Beitrag erf&uuml;llt mindestens einen hier ausgew&auml;hlten Filter:'; break;
-            case 'lang_multiFilter': return 'Dr&uuml;cke Strg um mehr als einen Filter auszuw&auml;hlen.<br>Achtung: &Auml;nderungen sind sofort wirksam!'; break;
-            case 'lang_newDebate': return 'Neue Debatte'; break;
-            case 'lang_newDebateCreate': return 'Neue Debatte erstellen'; break;
-            case 'lang_newSolution': return 'Neue L&ouml;sung erstellen'; break;
-            case 'lang_noArgPro': return 'Noch keine Argumente daf&uuml;r'; break;
-            case 'lang_noArgNeut': return 'Noch keine Antworten'; break;
-            case 'lang_noArgCon': return 'Noch keine Argumente dagegen'; break;
-            case 'lang_ok': return 'OK'; break;
-            case 'lang_solutionAdd': return 'L&ouml;sung hinzuf&uuml;gen'; break;
-            case 'lang_solutionDescription': return 'Bitte beschreibe deine L&ouml;sung in einigen S&auml;tzen!'; break;
-            case 'lang_textComment': return 'Beschreibung des Kommentars: '; break;
-            case 'lang_textDebate': return 'Beschreibung der Debatte: '; break;
-            case 'lang_textSolution': return 'Beschreibung der L&ouml;sung: '; break;
-            case 'lang_titleComment': return 'Titel des Kommentars: '; break;
-            case 'lang_titleDebate': return 'Titel der Debatte: '; break;
-            case 'lang_titleSolution': return 'Titel der L&ouml;sung: '; break;
-            case 'lang_topics': return 'Themenbereiche: '; break;
-            case 'lang_yArg': return 'dein Argument'; break;
-            case 'lang_yAnsw': return 'deine Antwort'; break;
+            case 'lang_errorLayer': return 'Es ist ein schwerer Fehler aufgetreten. Bitte Klicke auf Abbrechen!';
+            case 'lang_errorDebateTitle': return 'Bitte gib einen Titel f&uuml;r deine Debatte an!';
+            case 'lang_filter': return 'W&auml;hle die gew&uuml;nschten Filter aus';
+            case 'lang_filterSelect': return 'Filter einstellen';
+            case 'lang_helpNewComment': return '<p>Bitte beschreibe dein neues Argument sachlich und verst&auml;ndlich und wiederhole keine Punkte, die bereits genannt wurden.</p><p>Der <b>Titel</b> ist die Kurzfassung deines Argumentes in wenigen Worten. Details und Quellen kannst du in der <b>Beschreibung</b> erl&auml;utern. Du kannst auch Argumente hinzuf&uuml;gen, die nicht deine pers&ouml;nliche Meinung st&uuml;tzen.</p>';
+            case 'lang_helpNewDebate': return '<p>Bitte beschreibe deine neue Debatte sachlich, neutral und verst&auml;ndlich und stelle sicher, dass diese Debatte nicht bereits gef&uuml;hrt wird.</p><p>Der <b>Titel</b> ist die Kurzfassung der Debatte in wenigen Worten. Details und Quellen kannst du in der <b>Beschreibung</b> erl&auml;utern. Bitte f&uuml;ge die Debatte auch einem oder mehreren <b>Themenbereichen</b> hinzu, sodass sie leicht gefunden werden kann.</p>';
+            case 'lang_helpNewSolution': return '<p>Bitte beschreibe deine neue L&ouml;sung sachlich, neutral und verst&auml;ndlich und wiederhole keine L&ouml;sungen, die bereits genannt wurden.</p><p>Der <b>Titel</b> ist die Kurzbeschreibung deiner L&ouml;sung in wenigen Worten. Details und Quellen kannst du in der <b>Beschreibung</b> erl&auml;utern. Du kannst auch L&ouml;sungen hinzuf&uuml;gen, die du pers&ouml;nlich nicht unterst&uuml;tzt.</p>';
+            case 'lang_listNew': return 'Neu';
+            case 'lang_listUnsolved': return 'Ungel&ouml;st';
+            case 'lang_listLatest': return 'Aktuell';
+            case 'lang_logout': return 'Logout';
+            case 'lang_minFilter': return 'Beitrag erf&uuml;llt mindestens einen hier ausgew&auml;hlten Filter:';
+            case 'lang_multiFilter': return 'Dr&uuml;cke Strg um mehr als einen Filter auszuw&auml;hlen.<br>Achtung: &Auml;nderungen sind sofort wirksam!';
+            case 'lang_newDebate': return 'Neue Debatte';
+            case 'lang_newDebateCreate': return 'Neue Debatte erstellen';
+            case 'lang_newSolution': return 'Neue L&ouml;sung erstellen';
+            case 'lang_noArgPro': return 'Noch keine Argumente daf&uuml;r';
+            case 'lang_noArgNeut': return 'Noch keine Antworten';
+            case 'lang_noArgCon': return 'Noch keine Argumente dagegen';
+            case 'lang_ok': return 'OK';
+            case 'lang_solutionAdd': return 'L&ouml;sung hinzuf&uuml;gen';
+            case 'lang_solutionDescription': return 'Bitte beschreibe deine L&ouml;sung in einigen S&auml;tzen!';
+            case 'lang_textComment': return 'Beschreibung des Kommentars: ';
+            case 'lang_textDebate': return 'Beschreibung der Debatte: ';
+            case 'lang_textSolution': return 'Beschreibung der L&ouml;sung: ';
+            case 'lang_titleComment': return 'Titel des Kommentars: ';
+            case 'lang_titleDebate': return 'Titel der Debatte: ';
+            case 'lang_titleSolution': return 'Titel der L&ouml;sung: ';
+            case 'lang_topics': return 'Themenbereiche: ';
+            case 'lang_yArg': return 'dein Argument';
+            case 'lang_yAnsw': return 'deine Antwort';
             default: return 'undefined';
-        }; break;
+        };
+        break;
     case 'en_GB':
         switch ( lang ) {
-            case 'lang_allFilter': return 'Contribution satisfies all selected filters:'; break;
-            case 'lang_answer': return 'Answer'; break;
-            case 'lang_argPro': return 'Add pro argument'; break;
-            case 'lang_argNeut': return 'Add new answer'; break;
-            case 'lang_argCon': return 'Add contra argument'; break;
-            case 'lang_author': return 'Author'; break;
-            case 'lang_back': return 'back'; break;
-            case 'lang_cancel': return 'cancel'; break;
-            case 'lang_createArgumentPro': return 'Create new pro argument:'; break;
-            case 'lang_createArgumentNeut': return 'Create new answer:'; break;
-            case 'lang_createArgumentCon': return 'Create new contra argument:'; break;
-            case 'lang_createcomment': return 'Create comment'; break;
-            case 'lang_createsolution': return 'Create Solution'; break;
-            case 'lang_debate': return 'Debate'; break;
-            case 'lang_description': return 'Description'; break;
+            case 'lang_allFilter': return 'Contribution satisfies all selected filters:';
+            case 'lang_answer': return 'Answer';
+            case 'lang_argPro': return 'Add pro argument';
+            case 'lang_argNeut': return 'Add new answer';
+            case 'lang_argCon': return 'Add contra argument';
+            case 'lang_author': return 'Author';
+            case 'lang_back': return 'back';
+            case 'lang_cancel': return 'cancel';
+            case 'lang_createArgumentPro': return 'Create new pro argument:';
+            case 'lang_createArgumentNeut': return 'Create new answer:';
+            case 'lang_createArgumentCon': return 'Create new contra argument:';
+            case 'lang_createcomment': return 'Create comment';
+            case 'lang_createsolution': return 'Create Solution';
+            case 'lang_debate': return 'Debate';
+            case 'lang_description': return 'Description';
             case 'lang_errorPartent':
-            case 'lang_errorLayer': return 'An fatal error occured. Please press cancelEs ist ein schwerer Fehler aufgetreten. Bitte Klicke auf Abbrechen!'; break;
-            case 'lang_errorDebateTitle': return 'Please enter a title for your debate!'; break;
-            case 'lang_filter': return 'Select the desired filter:'; break;
-            case 'lang_filterSelect': return 'select filters'; break;
-            case 'lang_helpText': return 'Supporttext!'; break;
-            case 'lang_logout': return 'Logout'; break;
-            case 'lang_minFilter': return 'Cntribution satisfies at least on of the selected filters:'; break;
-            case 'lang_multiFilter': return 'Press ctrl to select multiple filters.<br>Caution: Changes are in effect immediately'; break;
-            case 'lang_newDebate': return 'new Debate'; break;
-            case 'lang_newDebateCreate': return 'Create new debate'; break;
-            case 'lang_newSolution': return 'new Solution'; break;
-            case 'lang_noArgPro': return 'No pro arguments'; break;
-            case 'lang_noArgNeut': return 'No answers'; break;
-            case 'lang_noArgCon': return 'No contra arguments'; break;
-            case 'lang_ok': return 'OK'; break;
-            case 'lang_solutionAdd': return 'Add solution'; break;
-            case 'lang_solutionDescription': return 'Please describe your solution'; break;
-            case 'lang_text': return 'Text'; break;
-            case 'lang_title': return 'Title'; break;
-            case 'lang_topics': return 'Topics'; break;
-            case 'lang_yArg': return 'your argument'; break;
-            case 'lang_yAnsw': return 'your answer'; break;
+            case 'lang_errorLayer': return 'An fatal error occured. Please press cancelEs ist ein schwerer Fehler aufgetreten. Bitte Klicke auf Abbrechen!';
+            case 'lang_errorDebateTitle': return 'Please enter a title for your debate!';
+            case 'lang_filter': return 'Select the desired filter:';
+            case 'lang_filterSelect': return 'select filters';
+            case 'lang_helpText': return 'Supporttext!';
+            case 'lang_logout': return 'Logout';
+            case 'lang_minFilter': return 'Cntribution satisfies at least on of the selected filters:';
+            case 'lang_multiFilter': return 'Press ctrl to select multiple filters.<br>Caution: Changes are in effect immediately';
+            case 'lang_newDebate': return 'new Debate';
+            case 'lang_newDebateCreate': return 'Create new debate';
+            case 'lang_newSolution': return 'new Solution';
+            case 'lang_noArgPro': return 'No pro arguments';
+            case 'lang_noArgNeut': return 'No answers';
+            case 'lang_noArgCon': return 'No contra arguments';
+            case 'lang_ok': return 'OK';
+            case 'lang_solutionAdd': return 'Add solution';
+            case 'lang_solutionDescription': return 'Please describe your solution';
+            case 'lang_text': return 'Text';
+            case 'lang_title': return 'Title';
+            case 'lang_topics': return 'Topics';
+            case 'lang_yArg': return 'your argument';
+            case 'lang_yAnsw': return 'your answer';
             default: return 'undefined';
-        }; break;
+        };
+        break;
     }
 };
 }
