08cff9af472f66bf0cb5f453114df142513125d8
JustusW <justus@abi007.info>
2012-08-27 18:17:49 +0200

    Now checking for valid e-mail adresses. http://www.basdem.de/bugs/show_bug.cgi?id=6

--- www/class/user.class.php
+++ www/class/user.class.php
@@ -181,16 +181,81 @@ class User {
     }
 
     /**
+     * Checks if email is valid.
+     * Code slightly modified from http://www.linuxjournal.com/article/9585?page=0,3
+     * @return False if email is invalid, else true.
+     */
+    private static function validEmail($email) {
+        $atIndex = strrpos($email, "@");
+        if (is_bool($atIndex) && !$atIndex) {
+            return false;
+        } else {
+            $domain = substr($email, $atIndex+1);
+            $local = substr($email, 0, $atIndex);
+            $localLen = strlen($local);
+            $domainLen = strlen($domain);
+            if ($localLen < 1 || $localLen > 64) {
+                // local part length exceeded
+                return false;
+            } else if ($domainLen < 1 || $domainLen > 255) {
+                // domain part length exceeded
+                return false;
+            } else if ($local[0] == '.' || $local[$localLen-1] == '.') {
+                // local part starts or ends with '.'
+                return false;
+            }
+            else if (preg_match('/\\.\\./', $local)) {
+                // local part has two consecutive dots
+                return false;
+            } else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
+                // character not valid in domain part
+                return false;
+            } else if (preg_match('/\\.\\./', $domain)) {
+                // domain part has two consecutive dots
+                return false;
+            } else if (
+                !preg_match(
+                    '/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+                    str_replace("\\\\","",$local)
+                ) ) {
+                // character not valid in local part unless 
+                // local part is quoted
+                if ( !preg_match(
+                    '/^"(\\\\"|[^"])+"$/',
+                    str_replace("\\\\","",$local)) ) {
+                    return false;
+                }
+            }
+            if ( !( checkdnsrr($domain,"MX") 
+                || checkdnsrr($domain,"A") ) ) {
+                // domain not found in DNS
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /**
      * Checks if a necessary POST data has been sent with the request: email and password are required.
      * @return False if something is missing, else true.
      */
     private static function validatePost() {
         if ( !isset($_POST['email']) ) {
-            self::setError('Unbekannter Benutzername oder Passwort!<br>');
             return false;
         }
         if ( !isset($_POST['password']) ) {
+            return false;
+        }
+        if ( empty($_POST['email']) ) {
             self::setError('Unbekannter Benutzername oder Passwort!<br>');
+            return false;
+        }
+        if ( empty($_POST['password']) ) {
+            self::setError('Unbekannter Benutzername oder Passwort!<br>');
+            return false;
+        }
+        if ( !self::validEmail($_POST['email']) ) {
+            self::setError('E-Mail Adresse ist nicht g&uuml;ltig!<br>');
             return false;
         }
         return true;
